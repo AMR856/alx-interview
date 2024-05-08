e5910b10916a4d1b8ed12e7e783d14e6fac91520
ameralnas <amer.live477@gmail.com>
2024-05-08 18:48:42 +0300

    Go

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Pascal is calling"""
+
+
+def pascal_triangle(n):
+    """Here is a function that do stuff"""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [[1], [1, 1]]
+    my_list_of_lists = [[1], [1, 1]]
+    for i in range(2, n + 1, 1):
+        my_list_of_lists.append([1] * i)
+        for j in range(1, len(my_list_of_lists[i]) - 1, 1):
+            my_list_of_lists[i][j] = my_list_of_lists[i - 1][j - 1]
+            + my_list_of_lists[i - 1][j]
+
+    return my_list_of_lists
